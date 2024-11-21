d65c3139c63cd6058c7ef6b30ed5d81a30be8d5c
AlexMajumder <alexmajumder55@gmail.com>
2024-11-22 03:14:15 +0600

    enhance the card

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_components_hub/card.dart';
 import 'package:ui_components_hub/screens/slider_screen.dart';
 class WidgetApp extends StatefulWidget {
   const WidgetApp({super.key});
@@ -11,7 +12,7 @@ class _WidgetAppState extends State<WidgetApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SliderScreen(),
+      home: CardScreen(),//SliderScreen()
     );
   }
 }
--- lib/card.dart
+++ lib/card.dart
@@ -1,1 +1,30 @@
-class Card
+import 'package:flutter/material.dart';
+
+class CardScreen extends StatelessWidget {
+  const CardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card'),
+        ),
+        body: Center(
+          child: Card(
+            elevation: 150,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(126),
+            ),
+            color: const Color.fromRGBO(199, 70, 178, 4),
+            shadowColor: const Color.fromRGBO(197, 78, 2, 4),
+            child: const SizedBox(
+              height: 200,
+              width: 200,
+              child: Center(
+                child: Text('A card Showing here'),
+              ),
+            ),
+          ),
+        ));
+  }
+}
