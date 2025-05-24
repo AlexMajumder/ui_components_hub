7d701598fe6aeaad8b7d8680f9faf7b4e5080f23
AlexMajumder <alexmajumder55@gmail.com>
2025-05-24 21:31:54 +0600

    setup network caller

--- lib/card.dart
+++ lib/card.dart
@@ -6,25 +6,26 @@ class CardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Card'),
-        ),
-        body: Center(
-          child: Card(
-            elevation: 150,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(126),
-            ),
-            color: const Color.fromRGBO(199, 70, 178, 4),//
-            shadowColor: const Color.fromRGBO(197, 78, 2, 4),
-            child: const SizedBox(
-              height: 200,
-              width: 200,
-              child: Center(
-                child: Text('A card Showing here'),
-              ),
+      appBar: AppBar(
+        title: const Text('Card'),
+      ),
+      body: Center(
+        child: Card(
+          elevation: 150,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(126),
+          ),
+          color: const Color.fromRGBO(199, 70, 178, 4), //
+          shadowColor: const Color.fromRGBO(197, 78, 2, 4),
+          child: const SizedBox(
+            height: 200,
+            width: 200,
+            child: Center(
+              child: Text('A card Showing here'),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
